1c66c45174d1ae2f8b9394fea6acca6b90fc30f7
Bz-zik <narciss95@yandex.ru>
2017-12-08 00:25:17 +0300

    created User Entity

--- src/main/java/com/epam/enote/model/User.java
+++ src/main/java/com/epam/enote/model/User.java
@@ -1,8 +1,44 @@
 package com.epam.enote.model;
 
-public class User {
-    int id;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User extends Base {
+
+    @Basic
+    @Column(name = "login")
     String login;
+
+    @Basic
+    @Column(name = "password")
     String password;
+
+    @Basic
+    @Column(name = "name")
     String name;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
