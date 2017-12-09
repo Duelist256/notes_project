be255d9c9b0c8af88f5b00e53c16a6b149d80d8c
Ivan Seliukov <Ivan_Seliukov@epam.com>
2017-12-09 18:18:12 +0300

    Add UserService implementation

---
+++ src/main/java/com/epam/enote/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.epam.enote.service.impl;
+
+import com.epam.enote.model.User;
+import com.epam.enote.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User addUser(User user) {
+        throw new UnsupportedOperationException("Method isn't implemented yet");
+    }
+
+    @Override
+    public User updateUser(User user) {
+        throw new UnsupportedOperationException("Method isn't implemented yet");
+    }
+
+    @Override
+    public User getUserById(int id) {
+        throw new UnsupportedOperationException("Method isn't implemented yet");
+    }
+
+    @Override
+    public void deleteUser(User user) {
+        throw new UnsupportedOperationException("Method isn't implemented yet");
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        throw new UnsupportedOperationException("Method isn't implemented yet");
+    }
+}
